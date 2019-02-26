724910ac71bae35de9f7a1e404ba35c2fd675cab
wangding <408542507@qq.com>
2019-02-26 19:59:22 +0800

    calculate0.1

---
+++ rectangle.js
@@ -0,0 +1,24 @@
+$(function(){
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  console.log('asdasdasd');
+
+  $btnCal.click(function(){
+    var w = Number($width.val());
+    var h = Number($height.val());
+
+    var p = 2 * ( w + h);
+    var a = w + h;
+
+    $perimeter.val(p);
+    $area.val(a);
+
+    console.log(w);
+    console.log(h);
+    console.log(p);
+    console.log(a);
+  });
+});
